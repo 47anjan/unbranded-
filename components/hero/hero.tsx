d0f97d7f43d58068c61175d0d9c93541804546e9
47anjan <anjankarmakar15@gmail.com>
2025-02-03 05:15:08 +0600

    Refactor Hero component layout for improved responsiveness and styling

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -14,12 +14,12 @@ const Hero = () => {
     <section className="relative  md:min-h-screen grid grid-cols-1 lg:grid-cols-2">
       <div className="flex flex-col justify-center px-4 sm:px-6 lg:px-8 xl:px-16 max-w-2xl lg:max-w-none mx-auto">
         <div className="lg:max-w-xl pt-24 sm:pt-32 lg:pt-0  text-center md:text-start">
-          <div className="flex sm:hidden flex-col items-center justify-center gap-2 align-middle md:flex-row md:items-start mb-2">
-            <div className="z-10 flex -space-x-2 ">
+          <div className=" sm:flex   flex-col items-center justify- md:justify-start gap-2 align-middle flex-wrap md:flex-row md:items-start mb-2 md:mb-4">
+            <div className="z-10 flex justify-center md:justify-start  -space-x-4 rtl:space-x-reverse ">
               {imageUrls.map((url, index) => (
                 <Image
                   key={index}
-                  className="size-8 rounded-full border-2 border-white dark:border-gray-800 object-cover"
+                  className=" size-8 sm:size-12 rounded-full border-2 border-white dark:border-gray-800 object-cover"
                   src={url}
                   width={60}
                   height={60}
@@ -27,7 +27,8 @@ const Hero = () => {
                 />
               ))}
             </div>
-            <div className="flex  gap-2  items-center ">
+            <div className="flex mt-1 flex-row-reverse justify-center  sm:justify-center sm:flex-col   gap-1 items-center md:items-start">
+              <div className="font-semibold text-sm">127+ Happy Clients</div>
               <div className="flex gap-1">
                 <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
                 <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
@@ -35,7 +36,6 @@ const Hero = () => {
                 <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
                 <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
               </div>
-              <div className="font-semibold text-sm">127+ Happy Clients</div>
             </div>
           </div>
 
@@ -55,34 +55,6 @@ const Hero = () => {
               <button className="px-6 w-full h-fit max-w-48 mx-auto md:mx-0 py-3 border  rounded-lg   text-lg font-semibold leading-6 text-center whitespace-nowrap transition-colors duration-200 ease-in bg-[#21242a] text-white hover:bg-[#02a366] ">
                 Book a call
               </button>
-            </BoxReveal>
-            <BoxReveal boxColor={""} duration={0.7}>
-              <div className="hidden sm:flex flex-col items-center justify-center gap-2 align-middle flex-wrap md:flex-row md:items-start">
-                <div className="z-10 flex  -space-x-4 rtl:space-x-reverse">
-                  {imageUrls.map((url, index) => (
-                    <Image
-                      key={index}
-                      className="h-12 w-12 rounded-full border-2 border-white dark:border-gray-800 object-cover"
-                      src={url}
-                      width={60}
-                      height={60}
-                      alt={`Avatar ${index + 1}`}
-                    />
-                  ))}
-                </div>
-                <div className="flex flex-col  gap-1 items-center md:items-start">
-                  <div className="font-semibold text-sm">
-                    127+ Happy Clients
-                  </div>
-                  <div className="flex gap-1">
-                    <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
-                    <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
-                    <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
-                    <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
-                    <Star className="h-4 w-4 fill-orange-400 text-orange-400" />
-                  </div>
-                </div>
-              </div>
             </BoxReveal>
           </div>
         </div>
